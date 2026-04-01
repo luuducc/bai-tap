33c5598fe42b720784a784c49c0d0790a17311ba
iuuduc <luuduc1708@gmail.com>
2026-04-02 01:05:47 +0700

    add logic flow

---
+++ raw_input.js
@@ -0,0 +1,6 @@
+export const rawInputs = [
+  // op1: toán tử 1, op2: toán tử 2, ans: kết quả > < =
+  { op1: 2, op2: 3, ans: '<'},
+  { op1: 3, op2: 3, ans: '='},
+  { op1: 5, op2: 4, ans: '>'},
+]
