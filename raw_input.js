e6b0786ebf1fd632ac6d6b273ab6010d5c136557
luuduc <luuduc1708@gmail.com>
2026-04-02 08:47:09 +0700

    update compare logic

--- raw_input.js
+++ raw_input.js
@@ -1,6 +1,11 @@
 export const rawInputs = [
-  // op1: toán tử 1, op2: toán tử 2, ans: kết quả > < =
-  { op1: 2, op2: 3, ans: '<'},
-  { op1: 3, op2: 3, ans: '='},
-  { op1: 5, op2: 4, ans: '>'},
+  // op1: toán tử 1, op2: toán tử 2
+  { op1: 2, op2: 3 },
+  { op1: 3, op2: 3 },
+  { op1: 5, op2: 4 },
+  { op1: 1, op2: 2 },
+  { op1: 7, op2: 3 },
+  { op1: 2, op2: 6 },
+  { op1: 5, op2: 5 },
+  { op1: 4, op2: 4 },
 ]
